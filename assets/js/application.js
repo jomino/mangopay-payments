47c24296694fbd1ef3f5a5aaf88609a769755730
Jomino Domino <jomino.domino@gmail.com>
2019-09-20 09:10:40 +0200

    add submit action

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     
     var $main_el = $('.container.main');
     var $loading_el = $('.container.loader');
+    var $form_els = $('form');
 
     var defaultLoaderOptions = {
         size: 35,
@@ -14,6 +15,10 @@ $(document).ready(function(){
             $loading_el.LoadingOverlay( show, $.extend( options || {}, defaultLoaderOptions));
         }
     };
+
+    $form_els.on('submit', function (e) {
+        $('[type=submit]',$(this)).attr('disabled','disabled');
+    });
 
     if($loading_el.length){
         window.setTimeout(function(){
